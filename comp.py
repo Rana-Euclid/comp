fb28e73345f89c33f1babf90f98eeaf353b795bf
Rana Banerjee <rana.banerjee@euclidinnovations.com>
2021-02-15 12:56:10 +0530

    first version

---
+++ comp.py
@@ -0,0 +1,40 @@
+import cv2
+import time
+from simpleHRNet.SimpleHRNet import SimpleHRNet
+
+# Open the video file with cv2
+
+cap = cv2.VideoCapture('sample.mp4')
+fps = cap.get(cv2.cv2.CAP_PROP_FPS)
+frame_count = 0
+
+start_time = time.time()
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    if frame is None:
+        break
+    else:
+        frame_count+=1
+        res = f'{frame.shape[0]}X{frame.shape[1]}'
+    # Call skeleton frameworks
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #Save the frame and the skeletons in the list
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+end_time = time.time()
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
+# Caclulate performance stats
+proc_time = end_time-start_time
+
+print (f'{fps},{res},{frame_count},{proc_time},{frame_count/proc_time},{proc_time/frame_count}')
+
+# Save the images with the skeletons in the results folder
